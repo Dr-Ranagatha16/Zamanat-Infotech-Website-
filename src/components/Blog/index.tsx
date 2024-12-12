fb5e9a2a368434c2be0a362ee964bb35ab74c1ee
Hari123455678 <harisarma898@gmail.com>
2024-12-12 12:51:42 +0530

    Made changes in home page for portfolio

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,31 +1,3 @@
-import SectionTitle from "../Common/SectionTitle";
-import SingleBlog from "./SingleBlog";
-import blogData from "./blogData";
-
-const Blog = () => {
-  return (
-    <section
-      id="blog"
-      className="bg-gray-light dark:bg-bg-color-dark py-16 md:py-20 lg:py-28"
-    >
-      <div className="container">
-        <SectionTitle
-          title="Our Portfolio"
-          paragraph="Browse through our portfolio of successful website projects. See the diverse range of 
-industries we’ve worked with, from e-commerce stores to corporate websites."
-          center
-        />
-
-        <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
-          {blogData.map((blog) => (
-            <div key={blog.id} className="w-full">
-              <SingleBlog blog={blog} />
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
-};
+3
 
 export default Blog;
